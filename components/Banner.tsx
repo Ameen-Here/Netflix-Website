37a7148b109c68082f135ea9004537b7a9a5ed65
Ameen-Here <johnharry125@yahoo.com>
2023-05-26 20:58:35 +0530

    Added banner image

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,30 @@
-const Banner = () => {
+import Image from "next/image";
+import { Movie } from "../typings";
+import { useEffect, useState } from "react";
+import { baseUrl } from "../constants/movie";
+
+interface Props {
+  netflixOriginals: Movie[];
+}
+const Banner = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null);
+  useEffect(() => {
+    console.log(netflixOriginals);
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, []);
+  console.log(movie);
   return (
     <div>
-      {/* <div>
-        <Image />
-      </div> */}
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-[98.5vw]">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          alt={"Image of poster"}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,30 @@ import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 import Banner from "../components/Banner";
 import requests from "../utils/request";
+import { Movie } from "../typings";
 
-const Home = () => {
+interface Props {
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
+}
+
+const Home = ({
+  netflixOriginals,
+  trendingNow,
+  topRated,
+  actionMovies,
+  comedyMovies,
+  horrorMovies,
+  romanceMovies,
+  documentaries,
+}: Props) => {
+  console.log("netflixOriginals", netflixOriginals);
   return (
     <div className="relative h-screen  bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -16,7 +38,7 @@ const Home = () => {
       {/* Header */}
       <Header />
       <main>
-        <Banner />
+        <Banner netflixOriginals={netflixOriginals} />
         {/* Banner */}
         <section>
           {/* Row */}
